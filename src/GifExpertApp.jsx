90c1713ed898af91fd2665e0e4a3b91a93eaa457
luislordev <luislor.dev@gmail.com>
2023-02-08 19:57:47 +0100

    Implement component of add categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import { AddCategory } from './Components/AddCategory';
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    'Attack of titans',
+    'Sword art online',
+  ]);
+
+  const onAddCategory = (category) => {
+    if (categories.includes(category)) return;
+    setCategories([category, ...categories]);
+  };
+
   return (
     <>
+      {/* Titulo */}
       <h1>GifExpertAPP</h1>
+
+      {/* Buscador */}
+      <AddCategory
+        //   setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
+
+      {/* Listado de gifs */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
